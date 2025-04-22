0b5b65cfe47c3b0843ea1db7c83090f02f2a875a
TravisMa07 <travis07ma@gmail.com>
2025-04-22 13:08:53 -0400

    Implemented fully customizable screen layout
    Implemented CSS styling and HTML div to fully customize the layout of each screen. Whether the text content, image, etc on the left, right,  top, bottom, or middle. (WIP: each screen content customize to my personal information. Need to customize image and text.)

--- portfolio-screen.js
+++ portfolio-screen.js
@@ -56,7 +56,6 @@ export class PortfolioScreen extends DDDSuper(I18NMixin(LitElement)) {
         box-sizing: border-box;
         
       }
-
       
       .wrapper {
         margin: var(--ddd-spacing-2);
@@ -65,12 +64,39 @@ export class PortfolioScreen extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--portfolio-very-theme-label-font-size, var(--ddd-font-size-s));
       }
+
+      ::slotted(portfolio-screen){
+        height: 100vh;
+        width: 100vw;
+        box-sizing: border-box;
+      }
+
+      ::slotted(portfolio-screen[screenNumber="1"]) {
+        background-color: var(--ddd-theme-default-beaverBlue); 
+      }
+      ::slotted(portfolio-screen[screenNumber="2"]) {
+        background-color: var(--ddd-theme-default-nittanyNavy); 
+      }
+      ::slotted(portfolio-screen[screenNumber="3"]) {
+        background-color: var(--ddd-theme-default-navy40);
+      }
+      ::slotted(portfolio-screen[screenNumber="4"]) {
+        background-color: var(--ddd-theme-default-potentialMidnight); 
+      }
+      ::slotted(portfolio-screen[screenNumber="5"]) {
+        background-color: var(--ddd-theme-default-potential50);
+      }
+
+
+      
+     
     `];
   }
 
   // Lit render the HTML
   render() {
     return html`
+      <slot></slot>
     `;
   }
 }
